f508208a6d9a03ca208a558350b40412dadef6cd
Cupid Valentine <westerngod@126.com>
2023-06-05 23:01:12 +0800

    feat: add number widget component

--- packages/vanilla/src/lib/theme-default/NumberWidget.vue
+++ packages/vanilla/src/lib/theme-default/NumberWidget.vue
@@ -1,13 +1,26 @@
 <script setup lang="ts">
+import { toRefs } from 'vue'
+import type { CommonWidgetProps, EmitProps } from '../types'
 
+defineOptions({
+  name: 'NumberWidget'
+})
+
+const props = defineProps<CommonWidgetProps>()
+
+const emit = defineEmits<EmitProps>()
+
+const { value } = toRefs(props)
+
+const handleChange = (e: any) => {
+  const value = e.target.value
+  e.target.value = value.value
+  emit('change', value)
+}
 </script>
 
 <template>
-  <div>
-
-  </div>
+  <input type="number" :value="value" @change="handleChange" />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
