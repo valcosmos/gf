15d1137863319582d6d8af6d121da5bf9a6f90ff
Cupid Valentine <westerngod@126.com>
2023-06-05 23:10:27 +0800

    feat: add selection widget component

--- packages/vanilla/src/lib/theme-default/SelectionWidget.vue
+++ packages/vanilla/src/lib/theme-default/SelectionWidget.vue
@@ -1,13 +1,40 @@
 <script setup lang="ts">
+import { ref, toRefs, watch } from 'vue'
+import type { EmitProps, SelectWidgetProps } from '../types'
+import FormItem from './FormItem.vue'
 
+defineOptions({ name: 'SelectionWidget' })
+
+const props = defineProps<SelectWidgetProps>()
+
+const emit = defineEmits<EmitProps>()
+
+const currentValueRef = ref(props.value)
+
+const { options } = toRefs(props)
+
+watch(currentValueRef, (newValue) => {
+  if (newValue !== props.value) {
+    emit('change', newValue)
+  }
+})
+
+watch(
+  () => props.value,
+  (v) => {
+    if (v !== currentValueRef.value) {
+      currentValueRef.value = v
+    }
+  }
+)
 </script>
 
 <template>
-  <div>
-
-  </div>
+  <FormItem v-bind="props">
+    <select multiple v-model="currentValueRef">
+      <option v-for="(op, index) in options" :key="index" :value="op.value">{{ op.key }}</option>
+    </select>
+  </FormItem>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
