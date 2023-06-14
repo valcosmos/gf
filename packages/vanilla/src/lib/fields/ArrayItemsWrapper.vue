357ed80fb14b3af05bec5135f10642c23ec46b16
Cupid Valentine <westerngod@126.com>
2023-06-14 22:47:46 +0800

    feat: add array items wrapper

---
+++ packages/vanilla/src/lib/fields/ArrayItemsWrapper.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+type actionType = [index: number]
+
+interface ArrayItemsWrapperProps {
+  index: number
+}
+
+type emitType = {
+  add: actionType
+  delete: actionType
+  up: actionType
+  down: actionType
+}
+
+const props = defineProps<ArrayItemsWrapperProps>()
+
+const emits = defineEmits<emitType>()
+
+const slots = defineSlots<{ default(): any }>()
+</script>
+
+<template>
+  <div>
+    <div>
+      <button @click="() => emits('add', props.index)">新增</button>
+      <button @click="() => emits('delete', props.index)">删除</button>
+      <button @click="() => emits('up', props.index)">上移</button>
+      <button @click="() => emits('down', props.index)">下移</button>
+    </div>
+    <div>{{ slots.default?.() }}</div>
+  </div>
+</template>
+
+<style scoped></style>
